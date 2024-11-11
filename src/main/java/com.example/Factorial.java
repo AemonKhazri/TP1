99581db26a34bba2874bd9f63573aa1062e2e651
Aemon33 <khazriaymen93@gmail.com>
2024-11-11 22:03:05 +0100

    no html in the servlet and work so far

--- src/main/java/com.example/Factorial.java
+++ src/main/java/com.example/Factorial.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
@@ -17,31 +18,22 @@ public class Factorial extends HttpServlet {
     }
 
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html");
 
         String numberParam = request.getParameter("number");
-        if (numberParam == null || numberParam.isBlank() || Integer.parseInt(numberParam)==0) {
-            response.getWriter().println("<html><body><h1 style=\"color: red\"  >Please provide a valid number.</h1></body></html>");
+        if (numberParam == null || numberParam.isBlank() || Integer.parseInt(numberParam)<=0) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Please provide a valid number");
             return;
         }
 
         int number = Integer.parseInt(numberParam);
         long factorial = calculateFactorial(number);
 
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1> Factorial of " +number+" is :"+factorial + "</h1>");
-        out.println("</body></html>");
-    }
-    @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/plain");
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body> in post");
-        out.println("<h1> " +  request.getParameter("number")+ "</h1>");
-        out.println("</body></html>");
+        request.setAttribute("number",number);
+        request.setAttribute("factorial",factorial);
+
+      request.getRequestDispatcher("/result.jsp").forward(request, response);
     }
 }
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,8 @@
+
+<html>
+<body>
+<h2>Factorial Calculation Result</h2>
+<p>Factorial of <strong>${number}</strong> is: <strong>${factorial}</strong></p>
+<a href="index.jsp">Calculate another factorial</a>
+</body>
+</html>
